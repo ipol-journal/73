4a58d9df4c5c3196fdeb3178b44dbe9bc2d4004a
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2024-06-12 11:26:21 +0530

    refactored code

--- run.py
+++ run.py
@@ -23,32 +23,34 @@ p = subprocess.run(['psf_estim', '-p', '/workdir/bin/pattern_noise.pgm',
                     '-s', str(args.s), '-k', str(args.k), '-d', 'det_out.ppm', '-t', str(args.t),
                     '-o', 'psf.pgm', 'input_0.pgm', 'psf.txt'])
 
-with open('stdout.txt', 'r') as file:
-    stdout_text = file.read()
-if "No pattern was detected." in stdout_text:
-    with open('demo_failure.txt', 'w') as f:
-        f.write('Pattern Not Found. Are you sure that there is a pattern in the image? It may have not been detected if\
-                        the pattern covers only a small part of the image. Crop the image so that the pattern covers at\
-                        least half of the image and re-run. Otherwise, upload an image containing a pattern.')
-        sys.exit(0)
+try:
+    im = PIL.Image.open("psf.pgm")
+    # re adjust width and height to avoid visualization interpolation
+    width = 600
+    height = 600
+    # interpolate it by neareset neighbor
+    im = im.resize((width, height)) 
+    im.save("psf.png")
 
-elif "More than one pattern was detected." in stdout_text:
-    with open('demo_failure.txt', 'w') as f:
-        f.write('More than one pattern was detected. Crop the image surounding the\
-                    desired pattern and re-run.')
-        sys.exit(0)
-else:
-    with open('demo_failure.txt', 'w') as f:
-        f.write('Unknown Run Time Error')
+    im = PIL.Image.open("det_out.ppm")
+    im.save("det_out.png")
 
+except RuntimeError:
+    with open('stdout.txt', 'r') as file:
+        stdout_text = file.read()
+    if "No pattern was detected." in stdout_text:
+        with open('demo_failure.txt', 'w') as f:
+            f.write('Pattern Not Found. Are you sure that there is a pattern in the image? It may have not been detected if\
+                            the pattern covers only a small part of the image. Crop the image so that the pattern covers at\
+                            least half of the image and re-run. Otherwise, upload an image containing a pattern.')
+            sys.exit(0)
 
-im = PIL.Image.open("psf.pgm")
-# re adjust width and height to avoid visualization interpolation
-width = 600
-height = 600
-# interpolate it by neareset neighbor
-im = im.resize((width, height)) 
-im.save("psf.png")
-
-im = PIL.Image.open("det_out.ppm")
-im.save("det_out.png")
+    elif "More than one pattern was detected." in stdout_text:
+        with open('demo_failure.txt', 'w') as f:
+            f.write('More than one pattern was detected. Crop the image surounding the\
+                        desired pattern and re-run.')
+            sys.exit(0)
+    else:
+        with open('demo_failure.txt', 'w') as f:
+            f.write('Unknown Run Time Error')
+            sys.exit(0)
